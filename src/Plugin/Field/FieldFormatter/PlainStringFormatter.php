6bc67e56bfe97f84405b6e04db65fa11c7c052f9
Szabó Dénes <szabo.denes@internode.hu>
2018-02-22 15:56:08 +0100

    Issue #2892985 by Denes.Szabo: Ability to add a CSS class

--- src/Plugin/Field/FieldFormatter/PlainStringFormatter.php
+++ src/Plugin/Field/FieldFormatter/PlainStringFormatter.php
@@ -5,6 +5,7 @@ namespace Drupal\string_field_formatter\Plugin\Field\FieldFormatter;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Field\FormatterBase;
 use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Component\Utility\Html;
 
 /**
  * Plugin implementation of the 'plain_string_formatter' formatter.
@@ -29,9 +30,10 @@ class PlainStringFormatter extends FormatterBase {
    * {@inheritdoc}
    */
   public static function defaultSettings() {
-    return array(
+    return [
       'wrap_tag' => '_none',
-    ) + parent::defaultSettings();
+      'wrap_class' => '',
+    ] + parent::defaultSettings();
   }
 
   /**
@@ -57,10 +59,18 @@ class PlainStringFormatter extends FormatterBase {
   public function settingsForm(array $form, FormStateInterface $form_state) {
     $element = array();
     $element['wrap_tag'] = array(
-      '#title' => t('Wrap text in tag'),
+      '#title' => t('Field wrapper tag'),
       '#type' => 'select',
       '#default_value' => $this->getSetting('wrap_tag'),
       '#options' => $this->defaultWrapTagOptions(),
+    );
+
+    $element['wrap_class'] = array(
+      '#title' => t('Field class'),
+      '#type' => 'textfield',
+      '#maxlength' => 128,
+      '#default_value' => $this->getSetting('wrap_class'),
+      '#description' => t('Separate multiple classes with space or comma.'),
     );
 
     return $element;
@@ -79,7 +89,35 @@ class PlainStringFormatter extends FormatterBase {
       $summary[] = t('Wrap text with tag: @tag', array('@tag' => $wrap_tag));
     }
 
+    $class = $this->getSetting('wrap_class');
+    $class = $this->prepareClasses($class);
+    if ($class) {
+      $summary[] = $this->formatPlural(count($class),
+        t('Wrapper additional CSS class: @class.', array('@class' => implode('', $class))),
+        t('Wrapper additional CSS classes: @class.', array('@class' => implode(' ', $class)))
+      );
+    }
+    else {
+      $summary[] = t('No additional CSS class defined.');
+    }
     return $summary;
+  }
+
+  /**
+   * Build classes.
+   */
+  public function prepareClasses($class) {
+    $class = preg_replace('! !', ',', $class);
+    $all = explode(',', $class);
+    $prepared = [];
+    foreach ($all as $c) {
+      $c = trim($c);
+      if ($c) {
+        $prepared[] = Html::getClass($c);
+      }
+    }
+
+    return $prepared;
   }
 
   /**
@@ -92,12 +130,17 @@ class PlainStringFormatter extends FormatterBase {
     if ('_none' == $wrap_tag) {
       $wrap_tag = '';
     }
+    $class = $this->getSetting('wrap_class');
+    $class = $this->prepareClasses($class);
 
     foreach ($items as $delta => $item) {
       $elements[$delta] = array(
         '#type' => 'html_tag',
         '#tag' => $wrap_tag,
         '#value' => $item->value,
+        '#attributes' => [
+          'class' => $class,
+        ]
       );
     }
 
